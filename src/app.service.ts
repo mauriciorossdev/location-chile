28bdcc9cd1d843442895e2a1cf9ea1e4f97ad5a3
nullPointer.Ross <mauriciorossdev@gmail.com>
2023-11-21 17:21:53 -0300

    change msg res to api/v1/

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      message: 'Bienvenido a la API de Location Chile',
+      description:
+        'Esta API proporciona información sobre regiones y comunas de Chile.',
+      instructions:
+        'Para obtener información sobre regiones y comunas, visite la documentación de la API',
+      example: {
+        'Buscar region': 'GET /api/v1/regions/name/{name}',
+        'Buscar Comuna': 'GET /api/v1/communes/name/{name}',
+      },
+      documentation:
+        'https://location-chile-production-8adb.up.railway.app/docs',
+    };
   }
 }
